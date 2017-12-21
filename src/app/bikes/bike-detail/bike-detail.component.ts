edf6334d01676bd43c39e179ce51d8c9169ab669
sushasawa <33564483+sushasawa@users.noreply.github.com>
2017-12-21 14:45:51 +0530

    Google Map location issue fixed.
    Google Map location issue fixed.

--- src/app/bikes/bike-detail/bike-detail.component.ts
+++ src/app/bikes/bike-detail/bike-detail.component.ts
@@ -1,43 +1,61 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import {
+    Component,
+    OnInit,
+    Input
+} from '@angular/core';
+import {
+    Http,
+    Response
+} from '@angular/http';
 
-import {Bikes} from '../bikes.model';
+import {
+    Bikes
+} from '../bikes.model';
 
 @Component({
-  selector: 'app-bike-detail',
-  templateUrl: './bike-detail.component.html',
-  styleUrls: ['./bike-detail.component.css']
+    selector: 'app-bike-detail',
+    templateUrl: './bike-detail.component.html',
+    styleUrls: ['./bike-detail.component.css']
 })
 export class BikeDetailComponent implements OnInit {
-	@Input() bike:Bikes;
-	currentImage: any={};
-  lat: number = 18.9894007;
-  lng: number = 73.1175162;
-  constructor(private http: Http) { }
+    @Input() bike: Bikes;
+    bikeLocation: String = '';
+    currentImage: any = {};
+    lat: number = 18.9894007;
+    lng: number = 73.1175162;
+    constructor(private http: Http) {}
 
-  ngOnInit() {
-  	this.currentImage = this.bike.images[0];
-    this.getBikeMap().subscribe(
-        (response:Response) => {
-          this.createLatLng(response.json());
-        },
-        (error)=> console.log(error)
-      );
-  }
+    ngOnInit() {
 
-  changeImage(event){
-  	let target = event.target;
-  	this.currentImage = {image: target.currentSrc, tag: target.alt};
-  }
+    }
 
-  getBikeMap(){
-    return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + this.bike.current_location_name + '&key=AIzaSyCSPQUeqVIJlwi-mxJf3TqVCZMRGdYzpUE');
-  }
+    ngOnChanges() {
+        console.log(this.bike.current_location_name);
+        this.currentImage = this.bike.images[0];
+        this.getBikeMap().subscribe(
+            (response: Response) => {
+                this.createLatLng(response.json());
+            },
+            (error) => console.log(error)
+        );
+    }
+    changeImage(event) {
+        let target = event.target;
+        this.currentImage = {
+            image: target.currentSrc,
+            tag: target.alt
+        };
+    }
 
-  createLatLng(result){    
-    let locationCoo = result.results[0].geometry.location;
-    this.lat = locationCoo.lat;
-    this.lng = locationCoo.lng;
-  }
+    getBikeMap() {
+        return this.http.get('https://maps.googleapis.com/maps/api/geocode/json?address=' + this.bike.current_location_name + '&key=AIzaSyCSPQUeqVIJlwi-mxJf3TqVCZMRGdYzpUE');
+    }
+
+    createLatLng(result) {
+        console.log('called createLatLng');
+        let locationCoo = result.results[0].geometry.location;
+        this.lat = locationCoo.lat;
+        this.lng = locationCoo.lng;
+    }
 
 }
